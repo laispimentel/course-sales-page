d3896c72c3a4c961a715a35be8ab82b2786c46ae
Lais Pimentel <estudosdalah@gmail.com>
2025-12-18 19:41:08 -0300

    iniciação da section pra-quem, adicao das variaveis cor e fonte e o inicio do componente

---
+++ jornada-font-end/src/components/SectionPraQuem/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export function SectionPraQuem(){
+    return(
+        <section className='pra-quem-container'>
+        <div className='pra-quem-content'>
+        <h2>Para quem é a <span>Jornada Fron-end?</span></h2>
+        <p>Este curso foi desenhado para ser a sua porta de entrada definitiva para a construção de projetos. Se você nunca digitou uma linha de código, comece aqui: saia do zero absoluto e construa algo concreto e funcional em apenas três aulas, adquirindo a fundação prática que realmente importa. Se você já é um desenvolvedor atuante, nosso método condensado servirá como um acelerador valioso, oferecendo agilidade, revisão estratégica de fundamentos e um framework rápido para estruturar seus projetos de ponta a ponta. Em resumo, se você quer finalizar o seu primeiro projeto em tempo recorde, seja você um iniciante curioso ou um profissional experiente, este curso é o seu caminho.</p>
+        </div>
+        </section>
+    )
+}
